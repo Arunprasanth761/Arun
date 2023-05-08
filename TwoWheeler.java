6dc98c1bba7f9b3467a87e552a8b2d6b891d8cb5
Arunprasanth761 <101284570+Arunprasanth761@users.noreply.github.com>
2023-05-08 21:51:59 +0530

    Update TwoWheeler.java

--- TwoWheeler.java
+++ TwoWheeler.java
@@ -2,21 +2,19 @@ package week1.day2;
 
 public class TwoWheeler {
 
-	public static void main(String[] args) {
-	
 		int noOfWheels=2;
-		System.out.println(noOfWheels);
+		long chassisNumber=145881L;
 		short noOfMirrors=2;
-		System.out.println(noOfMirrors);
-		long chassisNumber=2451L;
-		System.out.println(chassisNumber);
-		boolean isPunctured=true;
-		System.out.println(isPunctured);
-		String bikeName="Hero Honda";
-		System.out.println(bikeName);
 		double runningKM=1057.5D;
-		System.out.println(runningKM);
-		
+		boolean isPunctured=true;
+		String bikeName="RoyalEnfield";
+	public static void main(String[] args) {
+		System.out.println(chassisNumber);
+		System.out.println(noOfWheels);
+		System.out.println(noOfMirrors);
+		System.out.println(isPunctured);
+		System.out.println(bikeName);
+		System.out.println(runningKM);	
 	}
 
 }
