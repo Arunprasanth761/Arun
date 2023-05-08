da09248285ff7fc5191af1995a90624b7878751c
Arunprasanth761 <101284570+Arunprasanth761@users.noreply.github.com>
2023-05-08 21:46:04 +0530

    Update Mobile.java

--- Mobile.java
+++ Mobile.java
@@ -1,22 +1,21 @@
 package week1.day2;
 
 public class Mobile {
+		String mobileBrandName="OPPOF7";
+		int modelNumber=336;
+		char mobileLogo='O';
+		short noOfMobilePiece=10;
+	        long mobileImeiNumber=351567422655517L;
+		float mobilePrice=9999.99F;
+		boolean isDamaged=true;
 
 	public static void main(String[] args) {
-		
-		String mobileBrandName="Samsung Galaxy A33";
 		System.out.println(mobileBrandName);
-		char mobileLogo='S';
 		System.out.println(mobileLogo);
-		short noOfMobilePiece=5;
 		System.out.println(noOfMobilePiece);
-		int modelNumber=336;
 		System.out.println(modelNumber);
-		long mobileImeiNumber=351567422655517L;
 		System.out.println(mobileImeiNumber);
-		float mobilePrice=29999.99F;
 		System.out.println(mobilePrice);
-		boolean isDamaged=true;
 		System.out.println(isDamaged);
 		
 	
